bd85d778af10e9b7e63aa8143ef63788dc265bb2
RHEZUS <ludivinseunda2@gmail.com>
2024-07-16 14:39:03 +0300

    solve tasks

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+""" MongoDB Operations with Python using pymongo """
 
 
 def list_all(mongo_collection):
-    """ lists all documents in a collection """
+    """ List all documents in Python """
     documents = mongo_collection.find()
 
     if documents.count() == 0:
