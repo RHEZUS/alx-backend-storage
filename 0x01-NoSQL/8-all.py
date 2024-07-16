d9e9ce8190fd7d3f1c9ffa26d7900133039bdefb
RHEZUS <ludivinseunda2@gmail.com>
2024-07-16 14:35:37 +0300

    solve tasks

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 def list_all(mongo_collection):
     """ lists all documents in a collection """
-    return [doc for doc in mongo_collection.find()]
+    return list(mongo_collection.find())
